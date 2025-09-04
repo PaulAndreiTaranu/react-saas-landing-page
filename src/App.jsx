060620637637b64003dd058c6275912890a71170
Paul Andrei Taranu <paulandreitaranu@gmail.com>
2025-09-04 13:51:33 +0100

    Fixed Tailwindcss migration from v3 to v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Header from './sections/Header'
+
 const App = () => {
     return (
-        <div>
-            <p>APP</p>
-            <h1 class='h1'>Hello world!</h1>
-            <h2 class='h2'>Hello world!</h2>
-            <h3 class='h3'>Hello world!</h3>
-        </div>
+        <main className='overflow-hidden'>
+            <Header />
+        </main>
     )
 }
 
