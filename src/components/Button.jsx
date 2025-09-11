3bb9eeea36de4c1c5fed554b0aa0cd8e361a715e
Paul Andrei Taranu <paulandreitaranu@gmail.com>
2025-09-11 13:44:23 +0100

    Added Features and Hero components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,20 +3,16 @@ import { Marker } from './Marker'
 
 const Button = ({ icon, children, href, containerClassName, onClick, markerFill }) => {
     const InnerText = () => (
-        <>
-            <span className='relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden'>
-                <span className='absolute -left-[1px]'>
-                    <Marker fill={markerFill} />
-                </span>
-                {icon && (
-                    <img src={icon} alt='circle' className='size-10 mr-5 object-contain z-10' />
-                )}
-                <span className='relative z-2 font-poppins base-bold text-p1 uppercase'>
-                    {children}
-                </span>
-                <span className='glow-before'></span>
+        <span className='relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden'>
+            <span className='absolute -left-[1px]'>
+                <Marker fill={markerFill} />
             </span>
-        </>
+            {icon && <img src={icon} alt='circle' className='size-10 mr-5 object-contain z-10' />}
+            <span className='relative z-2 font-poppins base-bold text-p1 uppercase'>
+                {children}
+            </span>
+            <span className='glow-before glow-after'></span>
+        </span>
     )
     return href ? (
         <a
