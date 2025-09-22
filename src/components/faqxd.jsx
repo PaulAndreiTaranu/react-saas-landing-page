f9997593fb64a786db4a78ff03c9804eb81fb31e
Paul Andrei Taranu <paulandreitaranu@gmail.com>
2025-09-22 13:54:06 +0100

    Finished FAQ Section

--- src/components/faqxd.jsx
+++ src/components/faqxd.jsx
@@ -5,26 +5,25 @@ import 'react-slidedown/lib/slidedown.css'
 
 const FaqItem = ({ item, index }) => {
     const [activeId, setActiveId] = useState(null)
+
     const active = activeId === item.id
-    const onGroupClick = () => {
-        setActiveId(activeId === item.id ? null : item.id)
-    }
 
     return (
         <div className='relative z-2 mb-16'>
             <div
-                className='group relative flex cursor pointer items-center justify-between gap-10 px-7'
-                onClick={onGroupClick}>
+                className='group relative flex cursor-pointer items-center justify-between gap-10 px-7'
+                onClick={() => {
+                    setActiveId(activeId === item.id ? null : item.id)
+                }}>
                 <div className='flex-1'>
                     <div className='small-compact mb-1.5 text-p3 max-lg:hidden'>
                         {index < 10 ? '0' : ''}
                         {index}
                     </div>
-
                     <div
                         className={clsx(
                             'h6 text-p4 transition-colors duration-500 max-md:flex max-md:min-h-20 max-md:items-center',
-                            active && 'mag-lg:text-p1',
+                            active && 'max-lg:text-p1',
                         )}>
                         {item.question}
                     </div>
@@ -40,7 +39,7 @@ const FaqItem = ({ item, index }) => {
             </div>
 
             <SlideDown>
-                {active && <div className='body-3 px-7 py-3.5'>{item.answer}</div>}
+                {activeId === item.id && <div className='body-3 px-7 py-3.5'>{item.answer}</div>}
             </SlideDown>
 
             <div
@@ -48,11 +47,10 @@ const FaqItem = ({ item, index }) => {
                     'g5 -bottom-7 -top-7 left-0 right-0 -z-1 rounded-3xl opacity-0 transition-opacity duration-500 absolute',
                     active && 'opacity-100',
                 )}>
-                <div className='g4 absolute inset-0.5 -z-1 rounded-3xl'></div>
+                <div className='g4 absolute inset-0.5 -z-1 rounded-3xl' />
                 <div className='absolute left-8 top-0 h-0.5 w-40 bg-p1' />
             </div>
         </div>
     )
 }
-
 export default FaqItem
